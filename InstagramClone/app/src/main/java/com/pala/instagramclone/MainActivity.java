da230cd4411b5c91843ad8539d41f8a09da03fe5
PALA <cihat_pala@hotmail.com>
2021-03-14 22:31:31 +0300

    BottomNavigationView Ex

--- InstagramClone/app/src/main/java/com/pala/instagramclone/MainActivity.java
+++ InstagramClone/app/src/main/java/com/pala/instagramclone/MainActivity.java
@@ -2,12 +2,31 @@ package com.pala.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.pala.instagramclone.Utils.*;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: starting");
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationViewEx Setup*/
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        ButtomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
     }
 }

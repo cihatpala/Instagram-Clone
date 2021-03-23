0d3204d69b1643124224ac29e6ca632759c25583
PALA <cihat_pala@hotmail.com>
2021-03-23 22:59:07 +0300

    TabLayout adding all Fragment with SectionPagerAdapter

--- InstagramClone/app/src/main/java/com/pala/instagramclone/Home/MainActivity.java
+++ InstagramClone/app/src/main/java/com/pala/instagramclone/Home/MainActivity.java
@@ -1,6 +1,7 @@
 package com.pala.instagramclone.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.tabs.TabLayout;
 import com.pala.instagramclone.R;
 import com.pala.instagramclone.Utils.*;
 
@@ -24,7 +26,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: starting");
+
         setupBottomNavigationView();
+        setupViewPager();
+    }
+
+    /**
+     * Responsible for adding the 3 tabs: Camera, Home, Messages
+     */
+
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment()); //index 0
+        adapter.addFragment(new HomeFragment()); //index 1
+        adapter.addFragment(new MessagesFragment()); //index 2
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_action_name);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
     }
 
     /**
